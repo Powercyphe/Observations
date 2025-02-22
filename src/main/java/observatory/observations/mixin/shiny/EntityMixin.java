49767bd30d104ad8ecc16f4accc840a1ee27c324
Shiny <leo.pernice1@gmail.com>
2025-02-22 19:11:18 +0100

    Resolved merge conflicts

--- src/main/java/observatory/observations/mixin/shiny/EntityMixin.java
+++ src/main/java/observatory/observations/mixin/shiny/EntityMixin.java
@@ -1,6 +1,5 @@
 package observatory.observations.mixin.shiny;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
@@ -13,16 +12,6 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-
-    @ModifyReturnValue(method = "hasNoGravity", at = @At(value = "RETURN"))
-    private boolean observations$shouldHaveNoGravity(boolean original) {
-        Entity entity = (Entity) (Object) this;
-
-        if (entity instanceof PlayerEntity player) {
-            return original || TraitComponent.get(player).hasTrait(Trait.WEIGHTLESS);
-        }
-        else return original;
-    }
 
     @WrapWithCondition(method = "move", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;fall(DZLnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)V"))
     private boolean observations$preventFall(Entity entity, double heightDifference, boolean onGround, BlockState state, BlockPos landedPosition) {
